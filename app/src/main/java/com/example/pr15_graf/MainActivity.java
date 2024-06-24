735eb7c2309922734919082accade9797e11cba3
missosmal <fadesteam8@gmail.com>
2024-06-25 00:13:48 +0500

    Определение пути.Graf

--- app/src/main/java/com/example/pr15_graf/MainActivity.java
+++ app/src/main/java/com/example/pr15_graf/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pr15_graf;
 
 import android.os.Bundle;
+import android.os.Environment;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,12 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        fileName = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS) + "/record.3gpp";
     }
 }
